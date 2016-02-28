a28c6227f6b79d2d56232ab37a5b58cb5fd2b6aa
digitalLumberjack <digitallumberjack@gmail.com>
2016-02-28 16:14:04 +0100

    do not recreate the key if it exsits

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@
 [ ! -z "${APP_HOST}" ] && sed -i "s/APP_HOST/${APP_HOST}/" /etc/nginx/conf.d/default.conf
 [ ! -z "${APP_PORT}" ] && sed -i "s/APP_PORT/${APP_PORT}/" /etc/nginx/conf.d/default.conf
 
-/usr/share/letsencrypt/letsencrypt-auto certonly --standalone -n --agree-tos --email ${LETSENCRYPT_EMAIL} -d ${VIRTUAL_HOST}
+if [[ ! -f "/etc/letsencrypt/live/${VIRTUAL_HOST}/fullchain.pem" ]];then 
+  /usr/share/letsencrypt/letsencrypt-auto certonly --standalone -n --agree-tos --email ${LETSENCRYPT_EMAIL} -d ${VIRTUAL_HOST}
+fi
 
 sed -i "s|SSL_CERTIFICATE|/etc/letsencrypt/live/${VIRTUAL_HOST}/fullchain.pem|" /etc/nginx/conf.d/default.conf
 sed -i "s|SSL_KEY|/etc/letsencrypt/live/${VIRTUAL_HOST}/privkey.pem|" /etc/nginx/conf.d/default.conf
